cd4ec0ed8d86ae83daea3c4765afcfbf7eebd645
Vitor Schutz <76212597+VitorSchutz@users.noreply.github.com>
2023-01-22 11:16:09 -0300

    Input de Atributos

---
+++ Ficha.py
@@ -0,0 +1,18 @@
+Força = 0
+Destreza = 0
+Constituição = 0
+Sabedoria = 0
+Carisma = 0
+
+print("Bem-Vindo ao programa de rolagem e desenvolvimento de fichas de D&D")
+print("==============================================================")
+print("Agora irei fazer algumas perguntas sobre o seu personagem, responda todas elas")
+
+Força = int(input("Qual o valor do seu atributo de Força?"))
+Destreza = int(input("Qual o valor do seu atributo de Destreza?"))
+Constituição = int(input("Qual o valor do seu atributo de Constituição?"))
+Sabedoria = int(input("Qual o valor do seu atributo de Sabedoria?"))
+Carisma = int(input("Qual o valor do seu atributo de Carisma?"))
+
+
+
